eaab41590e7454aea8d3849d3b1cc3961feeec3e
Dilek Gamze <dgamzersoy@outlook.com>
2021-09-24 16:38:50 +0300

    24.09

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginModel } from '../models/loginModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiUrl="https://localhost:44359/api/auth";
+
+  constructor(private httpClient:HttpClient) { }
+
+  login(loginModel:LoginModel){
+    return this.httpClient.post(this.apiUrl+"login",loginModel)
+  }
+
+  isAuthenticated(){
+    if(localStorage.getItem("token")){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
